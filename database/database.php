2c5c004fd49ffadc16a071bf90413088eac17d6b
shatyrbekova <perizat.shatyrbekova@gmail.com>
2021-08-30 16:29:31 +0200

    all discs have printed

--- database/database.php
+++ database/database.php
@@ -1,40 +1,80 @@
+<?php
 $database =[
     [
         'title' => 'New Jersey',
         'author' => 'Bon Jovi',
         'year' => 1988,
+        'genre'=> "Rock",
         'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
     ],
     [
         'title' => 'Live at Wembley 86',
         'author' => 'Queen',
         'year' => 1992,
+        'genre'=> "Pop",
         'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg'
     ],
     [
         'title' => 'Ten\'s Summoner\'s Tales',
         'author' => 'Sting',
         'year' => 1993,
+        'genre'=> "Pop",
         'poster' => 'https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg'
     ],
     [
         'title' => 'Steve Gadd band',
         'author' => 'Steve Gadd Band',
         'year' => 2018,
+        'genre'=> "Jazz",
         'poster' => 'https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg'
     ],
     [
         'title' => 'Brave new World',
         'author' => 'Iron Maiden',
         'year' => 2000,
-        'poster' => 'https://i.ebayimg.com/images/g/1GwAAOSwFytaKr7l/s-l300.jpg'
+        'genre'=> "Metal",
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/810nSIQOLiL._SY355_.jpg'
     ],
     [
         'title' => 'One more car, one more rider',
         'author' => 'Eric Clapton',
         'year' => 2002,
+        'genre'=> "Rock",
         'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg'
+    ],
+    [
+        'title' => 'Made in Japan',
+        'author' => 'Deep Purple',
+        'year' => 1972,
+        'genre'=> "Rock",
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51rggtPgmRL.jpg'
+
+    ],
+    [
+        'title' => 'And Justice for All',
+        'author' => 'Metallica',
+        'year' => 1988,
+        'genre'=> "Metal",
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81r3FVfNG3L._SY355_.jpg'
+
+    ],
+    [
+        'title' => 'Hard Wired',
+        'author' => 'Dave Weckl',
+        'year' => 1994,
+        'genre'=> "Jazz",
+        'poster' => 'https://img.discogs.com/KOBsqQwKiNKH-q927oHMyVdDzSo=/fit-in/596x596/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-6406665-1418464475-6120.jpeg.jpg'
+
+    ],
+    [
+        'title' => 'Bad',
+        'author' => 'Michael Jacjson',
+        'year' => 1987,
+        'genre'=> "Pop",
+        'poster' => 'https://m.media-amazon.com/images/I/71K9CbNZPsL._SS500_.jpg'
     ]
+
 ];
+?>
     
 
--- index.php
+++ index.php
@@ -8,13 +8,26 @@
 </head>
 <body>
 <?php
-
+// [
+//     'title' => 'New Jersey',
+//     'author' => 'Bon Jovi',
+//     'year' => 1988,
+//     'genre'=> "Rock",
+//     'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+// ]
 require_once __DIR__ ."/database/database.php";
 
 foreach($database as $disc){
-    echo "{$disc}<br/>";
-    echo $disc['poster'];
-    echo  $disc['title'];
+
+    
+      // echo "{$disc}<br/>";
+      echo  "<img class='poster' src ='{$disc['poster']}'><br/>";
+      echo  "{$disc['title']}<br/>";
+      echo  "{$disc['author']}<br/>";
+      echo  "{$disc['genre']}<br/>";
+      echo  "{$disc['year']}<br/>";
+ 
+  
     
 }
 
