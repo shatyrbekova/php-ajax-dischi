e6eceb2b0d09aeab89a972f9d97741683df99e20
shatyrbekova <perizat.shatyrbekova@gmail.com>
2021-08-30 17:01:53 +0200

    created new html page

--- index.php
+++ index.php
@@ -4,34 +4,34 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Discs</title>
 </head>
 <body>
+<div class="container">
 <?php
-// [
-//     'title' => 'New Jersey',
-//     'author' => 'Bon Jovi',
-//     'year' => 1988,
-//     'genre'=> "Rock",
-//     'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
-// ]
+
 require_once __DIR__ ."/database/database.php";
+
 
 foreach($database as $disc){
 
     
-      // echo "{$disc}<br/>";
-      echo  "<img class='poster' src ='{$disc['poster']}'><br/>";
-      echo  "{$disc['title']}<br/>";
-      echo  "{$disc['author']}<br/>";
-      echo  "{$disc['genre']}<br/>";
-      echo  "{$disc['year']}<br/>";
- 
+      $poster = "<img class='poster' src ='{$disc['poster']}'><br/>";
+      $title = "{$disc['title']}<br/>";
+      $author ="{$disc['author']}<br/>";
+      $genre= "{$disc['genre']}<br/>";
+      $year =  "{$disc['year']}<br/>";
+
+     echo " <main class='albums'> 
+            $poster
+            $title
+            $author
+            $genre
+            $year
+     </main>";
   
-    
-}
-
-
 ?>
+};
+</div>
 </body>
 </html>
